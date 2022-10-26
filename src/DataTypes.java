61ccee92cb20474489462977d5c4d128eb4e5f52
juliancochran <julian.p.cochran@gmail.com>
2022-10-26 14:18:16 -0400

    Sample code covered in class on Wednesday 1/26/2022 Day 4

---
+++ src/DataTypes.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class DataTypes {
+    private double coolFactor;
+
+    private final int YEAR = 365;
+    private final int LEAP_YEAR = YEAR + 1;
+
+    //public void printCoolness() {
+        //System.out.println(main.coolFactor);
+    //}
+
+    public void canIUpdateLeapYear()    {
+        //LEAP_YEAR += 1;
+    }
+
+    public static int digitSwap(int num)   {
+        int ones = num % 10;
+        num -= ones;
+        int tens = num % 100;
+        num -= tens;
+        tens /= 10;
+        num += (ones*10);
+        num += tens;
+
+        return num;
+
+
+
+        //System.out.println("DEBUG: " + ones + " " + tens);
+        //return 0;
+    }
+
+    public static void main(String[] args) {
+        //int hours, double pay;
+        int num = 123;
+        System.out.println(num + "\t" + digitSwap(num));
+        num = 30;
+        System.out.println(num + "\t" + digitSwap(num));
+        num = 5;
+        System.out.println(num + "\t" + digitSwap(num));
+
+        /*double coolFactor = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+        if(name.toLowerCase().equals("julian")) {
+            coolFactor = 1.0;
+        }
+        System.out.println("Your name is: " + name);
+        System.out.println("And your coolness is " + coolFactor);*/
+    }
+}
